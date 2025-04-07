396920c9a04221ef4949cf5f8080f5741f138ed3
muhammadjon <muhammadjon@gmail.com>
2025-04-07 10:13:27 +0500

    refactor: restructure OnlineUsers component layout for improved user display

--- src/components/OnlineUsers.jsx
+++ src/components/OnlineUsers.jsx
@@ -5,12 +5,23 @@ function OnlineUsers() {
 
   return (
     <div className="main-container mt-5">
-      {users &&
-        users.map((u) => (
-          <h1 key={u.id}>
-            {u.displayName} - {u.online ? "online" : "offline"}
-          </h1>
-        ))}
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="flex flex-col gap-5">
+          {users &&
+            users.map((u) => (
+              <div key={u.id} className="flex">
+                <img
+                  src={u.photoURL}
+                  className="max-w-12 rounded-lg shadow-2xl mr-3"
+                />
+                <h2 className="text-xl font-bold mt-2 btn btn-outline">
+                  {u.displayName} - {u.online ? "online" : "offline"}
+                </h2>
+                <div className="divider"></div>
+              </div>
+            ))}
+        </div>
+      </div>
     </div>
   );
 }
